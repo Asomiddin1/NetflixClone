cc0e10585ff5f7d8d467274d09ff539390026b83
Asomiddin <asomiddin320@gmail.com>
2025-01-28 19:56:40 +0500

    new design

--- js/main.js
+++ js/main.js
@@ -1,63 +1,35 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
 
-
-const apiKey = '534ba8e97f8b994e08f5c6e8b8c6c364'
-const baseUrl = "https://api.themoviedb.org/3";
-const imageBaseUrl = "https://image.tmdb.org/t/p/w500";
-const defaultPoster = "https://via.placeholder.com/500x750?text=No+Image";
-
-// Mashhur kinolarni olish funksiyasi
-async function getMovies() {
-  try {
-    const response = await fetch(`${baseUrl}/movie/popular?api_key=${apiKey}&language=en-US&page=1`);
-    const data = await response.json();
-
-    const movies = data.results;
-    const moviesContainer = document.getElementById("moviesContainer");
-    console.log(movies);
-    
-    moviesContainer.innerHTML = ""; // Avvalgi ma'lumotlarni tozalash
-
-    movies.forEach(movie => {
-      const posterPath = movie.poster_path ? `${imageBaseUrl}${movie.poster_path}` : defaultPoster;
-
-      const movieDiv = document.createElement("div");
-      movieDiv.className = "movie";
-
-      movieDiv.innerHTML = `
-       <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
-        <h3>${movie.title}</h3>
-        <p>${movie.overview}</p>
-        <button onclick="getMovieTrailer(${movie.id}, this)">Trailer ko'rish</button>
-        <a href="#" target="_blank" class="trailerLink" style="display: none;">YouTube Trailer</a>
-      `;
-
-      moviesContainer.appendChild(movieDiv);
-    });
-  } catch (error) {
-    console.error("Xato yuz berdi:", error);
-  }
-}
-
-// Trailerni olish funksiyasi
-async function getMovieTrailer(movieId, button) {
-  try {
-    const response = await fetch(`${baseUrl}/movie/${movieId}/videos?api_key=${apiKey}&language=en-US`);
-    const data = await response.json();
-
-    const trailers = data.results.filter(video => video.type === "Trailer" && video.site === "YouTube");
-    if (trailers.length > 0) {
-      const trailerUrl = `https://www.youtube.com/watch?v=${trailers[0].key}`;
-      const trailerLink = button.nextElementSibling;
-      trailerLink.href = trailerUrl;
-      trailerLink.style.display = "inline";
-      trailerLink.textContent = "Trailer-ni ko'rish";
+arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 270);
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
     } else {
-      alert("Trailer topilmadi.");
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
     }
-  } catch (error) {
-    console.error("Xato yuz berdi:", error);
-  }
-}
+  });
 
-// Dastur ishga tushganda chaqiriladi
-getMovies();
+  console.log(Math.floor(window.innerWidth / 270));
+});
+
+//TOGGLE
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
+);
+
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
